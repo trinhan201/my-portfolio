1b71017110144d047a00015f227d28ba77a8ec39
trinhan201 <trinhphieuan@gmail.com>
2022-11-28 18:55:21 +0700

    Handle form #1

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,19 @@
+import { useState } from 'react';
+
 function Form({ children }) {
+    const [inputValue, setInputValue] = useState({
+        fullName: '',
+        email: '',
+        subject: '',
+        message: '',
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(inputValue);
+    };
     return (
-        <form>
+        <form name="contact" method="post" onSubmit={handleSubmit}>
             <div className="flex">
                 <div className="flex-1 mt-4 mr-[8px]">
                     <input
@@ -8,18 +21,20 @@ function Form({ children }) {
                         type="text"
                         name="fullName"
                         placeholder="Full Name"
-                        value=""
+                        value={inputValue.fullName}
                         required
+                        onChange={(e) => setInputValue((prev) => ({ ...prev, fullName: e.target.value }))}
                     />
                 </div>
                 <div className="flex-1 mt-4 ml-[8px]">
                     <input
                         className="p-3 w-full outline outline-1 outline-[#1f2044] focus:outline-[#88b4dc]"
-                        type="text"
+                        type="email"
                         name="email"
                         placeholder="Email"
-                        value=""
+                        value={inputValue.email}
                         required
+                        onChange={(e) => setInputValue((prev) => ({ ...prev, email: e.target.value }))}
                     />
                 </div>
             </div>
@@ -29,7 +44,8 @@ function Form({ children }) {
                     type="text"
                     name="subject"
                     placeholder="Subject"
-                    value=""
+                    value={inputValue.subject}
+                    onChange={(e) => setInputValue((prev) => ({ ...prev, subject: e.target.value }))}
                 />
             </div>
             <textarea
@@ -39,8 +55,13 @@ function Form({ children }) {
                 cols="30"
                 placeholder="Message"
                 required
+                value={inputValue.message}
+                onChange={(e) => setInputValue((prev) => ({ ...prev, message: e.target.value }))}
             ></textarea>
-            <button className="rounded py-2 px-5 font-semibold opacity-90 hover:opacity-100 mt-2 cursor-pointer">
+            <button
+                className="bg-[#88b4dc] text-white rounded py-2 px-5 font-semibold opacity-90 hover:opacity-100 mt-2 cursor-pointer"
+                type="submit"
+            >
                 Send
             </button>
             {children}
