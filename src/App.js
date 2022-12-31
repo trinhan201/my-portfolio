787475be06c554290e41c6a62de13a45850203a6
trinhan201 <trinhphieuan@gmail.com>
2022-12-31 15:16:17 +0700

    Add page loading animation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import { useState, useEffect } from 'react';
 import { Header, Home, About, Skills, Projects, Contact, Footer } from '~/components/';
+import PacmanLoader from 'react-spinners/PacmanLoader';
 
 function App() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(true);
+        const loadingId = setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+
+        return () => clearTimeout(loadingId);
+    }, []);
+
     return (
-        <div className="">
-            <Header />
-            <Home />
-            <About />
-            <Skills />
-            <Projects />
-            <Contact />
-            <Footer />
+        <div className="App">
+            {loading ? (
+                <div className="flex items-center justify-center w-full h-screen">
+                    <PacmanLoader color={'#ffdf00'} loading={loading} size={30} />
+                </div>
+            ) : (
+                <>
+                    <Header />
+                    <Home />
+                    <About />
+                    <Skills />
+                    <Projects />
+                    <Contact />
+                    <Footer />
+                </>
+            )}
         </div>
     );
 }
